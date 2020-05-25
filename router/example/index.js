87e92813934097c71fe44fd873afe3761f2535de
pmh-only <pmhstudio.pmh@gmail.com>
2020-05-25 11:10:25 +0900

    Moded

--- router/example/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = { _root: '/example', _socket: false, _cors: true, _host: 'direct.trinets.xyz', _parser: [], ready }
-
-function ready (app) {
-  console.log('example is loaded')
-  app.get('/', (_req, res) => res.send('example!'))
-}
